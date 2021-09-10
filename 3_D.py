c21c699bbb8ed7a23092f85e6549fe2a1a2520db
fllnrn <lazy@home.local>
2021-09-10 13:18:49 +0300

    3D

--- 3_D.py
+++ 3_D.py
@@ -1,20 +1,18 @@
 n = int(input())
 belatrice_say = input()
-wrong = set()
-answer = set()
+answer = set(range(1, n+1))
 while belatrice_say != 'HELP':
-    nums = list(map(int, belatrice_say.split()))
+    nums = set(map(int, belatrice_say.split()))
     august_say = input()
     if august_say == 'NO':
         for num in nums:
-            if not (num in wrong):
-                if num in answer:
-                    answer.remove(num)
-                wrong.add(num)
+            if num in answer:
+                answer.remove(num)
     else:
-        for num in nums:
-            if not (num in wrong):
-                answer.add(num)
+        if answer:
+            for num in list(answer):
+                if not(num in nums):
+                    answer.remove(num)
     belatrice_say = input()
 
 answer = list(answer)
