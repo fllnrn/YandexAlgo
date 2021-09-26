55c2aa1b8d0a41d56b4fb43e138e3055a4452ec7
fllnrn <lazy@home.local>
2021-09-27 01:02:42 +0300

    котики

--- 7/7_D.py
+++ 7/7_D.py
@@ -1,1 +1,28 @@
+ITSCAT = 0
+END = 1
+START = -1
+
 n, m = map(int, input().split())
+events = [ (x, ITSCAT, 0) for x in map(int, input().split())]
+
+answer = [0]*m
+for i in range(m):
+    l, r = map(int, input().split())
+    events.append((l, START, i))
+    events.append((r, END, i))
+
+events.sort()
+
+
+cats = 0
+for event in events:
+    x, etype, index = event
+    if etype == START:
+        answer[index] = -cats
+    elif etype == END:
+        answer[index] += cats
+    elif etype == ITSCAT:
+        cats += 1
+        
+
+print(*answer)
